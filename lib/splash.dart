cd334f95786af47204fd6e9095f01949def6a17e
riyasklb <riyasklb89@gmail.com>
2022-08-31 14:07:19 +0530

    first commit

---
+++ lib/splash.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+        children: [Image.asset('assets/'), Text('data')],
+      )),
+    );
+  }
+}
